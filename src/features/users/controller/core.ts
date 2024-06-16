decae2464b5c5fcf783396783cc9a90bf9699548
Luan Almeida <luan.almeida@jbtec.com.br>
2024-06-16 15:27:12 -0300

    feat: create user in route core

--- src/features/users/controller/core.ts
+++ src/features/users/controller/core.ts
@@ -1,16 +1,28 @@
 import debug from "debug";
 import { Router } from "express";
+import { validateBodyForCreateUser } from "./rules";
+import { model } from "..";
 
 const logger = debug("features:users:controller");
 const route = Router();
 
-route.get("/", (_req, res) => {
-  logger('GET "/"');
-  res.status(200).json({
-    code: "users/ok",
-    message: "list users",
-    args: [],
-  });
+route.post("/", validateBodyForCreateUser, async (req, res) => {
+  try {
+    const result = await model.createUser({
+      user: req.body,
+    });
+    res.json({
+      code: "api.live.user.create.success",
+      message: "user created",
+      data: result,
+    });
+  } catch (error) {
+    logger("Create user failed: %O", error);
+    res.status(500).json({
+      code: "api.live.user.create.failed",
+      message: `Create user failed: ${error}`,
+    });
+  }
 });
 
 export { route };
