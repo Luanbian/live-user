4995c5fd299dbeab0c9f8283ad990b1b9084ae7b
Luan Almeida <luan.almeida@jbtec.com.br>
2024-06-16 18:30:22 -0300

    fix: implement stopManager and isAllDone verification to avoid JavaScript heap out of memory

--- src/features/users/watcher/uniqueExecution.ts
+++ src/features/users/watcher/uniqueExecution.ts
@@ -1,4 +1,4 @@
-export type UniqueExecutionCallback = () => Promise<void> | void;
+type UniqueExecutionCallback = () => Promise<void> | void;
 
 interface uniqueExecutionOptions {
   callback: UniqueExecutionCallback;
@@ -7,28 +7,37 @@ interface ExecutionManager {
   done: boolean;
   func: UniqueExecutionCallback;
 }
-
 interface Manager {
   queue: ExecutionManager[];
+  interval?: NodeJS.Timeout;
 }
 
 const manager: Manager = {
   queue: [],
 };
 
+const isAllDone = () =>
+  manager.queue.reduce((acc, cur) => cur.done && acc, true);
+
+const stopManager = (): void => {
+  if (manager.interval) clearInterval(manager.interval);
+  manager.interval = undefined;
+};
+
 const startManager = (): void => {
   setInterval(() => runTasks(), 1000);
 };
 
-async function runTasks() {
+const runTasks = async () => {
+  stopManager();
   const nextTask = manager.queue.filter((v) => !v.done);
   for (let x = 0; x < nextTask.length; x += 1) {
     const task = nextTask[x];
     await task.func();
     task.done = true;
   }
-  startManager();
-}
+  if (!isAllDone) startManager();
+};
 
 export const uniqueExecution = ({ callback }: uniqueExecutionOptions): void => {
   manager.queue.push({
