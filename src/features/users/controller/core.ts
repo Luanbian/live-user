dea8c6018ece7b0cd9a7aa7a74de5e1656b5d1a5
Luan Almeida <luan.almeida@jbtec.com.br>
2024-06-16 21:21:29 -0300

    feat: setup get route

--- src/features/users/controller/core.ts
+++ src/features/users/controller/core.ts
@@ -25,4 +25,23 @@ route.post("/", validateBodyForCreateUser, async (req, res) => {
   }
 });
 
+route.get("/", async (req, res) => {
+  try {
+    res.set("Content-Type", "text/event-stream");
+    res.set("Cache-Control", "no-cache");
+    res.set("Connection", "keep-alive");
+    res.flushHeaders();
+
+    const sendEvent = (data: any) => {
+      res.write(`data: ${JSON.stringify(data)}\n\n`);
+      return !(res.closed || res.destroyed);
+    };
+  } catch (error) {
+    logger("Get users failed: %O", error);
+    res.write(`event: error\n`);
+    res.write(`data: ${error}\n\n`);
+    res.end();
+  }
+});
+
 export { route };
